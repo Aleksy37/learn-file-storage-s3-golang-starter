3a99b0ac0980315c057e589850408f91b900a40e
Aleksy37 <aleksyveillette@gmail.com>
2026-09-23 14:47:40 -0400

    finished thumbnail upload handler and removed get thumbnail handler as the files are encoded with base64 and stored on the db now

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"encoding/base64"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -31,7 +33,45 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	mediaType := header.Header.Get("Content-Type")
+
+	bytes, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "could not read file", err)
+		return
+	}
+
+	
+	metadata, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "could not find video with that id", err)
+		return
+	}
+	if metadata.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "currently authenticated user is not the owner of this video", err)
+		return
+	}
+	
+	encodedBytes := base64.StdEncoding.EncodeToString(bytes)
+	dataUrl := fmt.Sprintf("data:%s;base64,%s", mediaType, encodedBytes)
+
+	metadata.ThumbnailURL = &dataUrl
+	err = cfg.db.UpdateVideo(metadata)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error updating video record",err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, metadata)
 }
